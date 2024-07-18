8ffedf4bdaa896a5285fef6ebe05cc4263b213dc
Gourav Sharma <125105311+gouravsharma1711@users.noreply.github.com>
2024-07-18 19:07:54 +0530

    Update day3.js

--- day3.js
+++ day3.js
@@ -51,7 +51,7 @@ switch(weekDayNumber){
         break;
     case 3: console.log("Wednesday");
         break;
-    case 4: console.log("Thrusday");
+    case 4: console.log("Thursday");
         break;
     case 5: console.log("Friday");
     break;
